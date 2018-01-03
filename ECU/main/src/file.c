a25e94d87d331d0254f840631b2f3f09ef5ad22d
crawler-john <895961454@qq.com>
2018-01-03 13:52:54 +0800

    1.更改逆变器最大台数 2.更改远程控制WIFI可能出现的接收不到情况 3.去除多余的打印信息
    Signed-off-by: crawler-john <895961454@qq.com>

--- ECU/main/src/file.c
+++ ECU/main/src/file.c
@@ -402,7 +402,7 @@ int get_id_from_file(inverter_info *firstinverter)
 	{
 		while(NULL != fgets(data,200,fp))
 		{
-			print2msg(ECU_DBG_MAIN,"ID",data);
+			//print2msg(ECU_DBG_MAIN,"ID",data);
 			memset(list,0,sizeof(list));
 			splitString(data,list);
 			//判断是否存在该逆变器
@@ -475,7 +475,7 @@ int get_id_from_file(inverter_info *firstinverter)
 			
 			inverter++;
 			num++;
-			if(num >= 50)
+			if(num >= MAXINVERTERCOUNT)
 			{
 				break;
 			}
@@ -494,13 +494,7 @@ int get_id_from_file(inverter_info *firstinverter)
 
 	inverter = firstinverter;
 	printmsg(ECU_DBG_MAIN,"--------------");
-	for(i=1; i<=num; i++,inverter++)
-	{
-		if(inverter->shortaddr == 0)
-		{
-			printmsg(ECU_DBG_MAIN,inverter->id);
-		}
-	}
+
 	
 	return num;
 }
--- components/DeviceDrivers/usart5.c
+++ components/DeviceDrivers/usart5.c
@@ -199,7 +199,7 @@ void WIFI_GetEvent(void)
 		if(eStateMachine == EN_RECV_ST_GET_SCOKET_HEAD)
 		{
 			while(pos < Cur)
-      {
+      		{
 				if(1 == pos)   //'a'
 				{
 						TIM3_Int_Init(499,7199);
@@ -428,7 +428,7 @@ void WIFI_GetEvent(void)
 					//SEGGER_RTT_printf(0, "LENGTH11111 : %d\n",PackLen);
 					//计算长度
 					eStateMachine = EN_RECV_ST_GET_A_DATA;
-					delayMS(100);
+					delayMS(30);
 					TIM3_Int_Init(499,7199);//10Khz的计数频率，计数到5000为500ms 打开定时器
 
 					break;
@@ -440,6 +440,7 @@ void WIFI_GetEvent(void)
 		//Continue to receive data
 		if(eStateMachine == EN_RECV_ST_GET_A_DATA)
 		{
+			TIM3_Int_Deinit();
 			pos = 0;
 			while(pos < Cur)
       		{
@@ -447,6 +448,7 @@ void WIFI_GetEvent(void)
 				if((PackLen - 3) == pos)   //接收数据长度结束
 				{
 					eStateMachine = EN_RECV_ST_GET_A_END;
+					TIM3_Int_Init(499,7199);
 					break;
 				}
 				pos++;
@@ -584,12 +586,14 @@ void WIFI_GetEvent(void)
 		//Continue to receive data
 		if(eStateMachine == EN_RECV_ST_GET_B_DATA)
 		{
+			TIM3_Int_Deinit();
 			pos = 0;
 			while(pos <= Cur)
       {
 				if(PackLen == pos)   //接收数据长度结束
 				{
 					eStateMachine = EN_RECV_ST_GET_B_END;
+					TIM3_Int_Init(499,7199);
 					break;
 				}
 				pos++;
@@ -623,9 +627,9 @@ void WIFI_GetEvent(void)
 		{
 			//SEGGER_RTT_printf(0, "EN_RECV_ST_GET_C_HEAD\n");
 			// check for the start character(SYNC_CHARACTER)
-			// also check it's not arriving the end of valid data
-      		while(pos < Cur)
-      		{
+      // also check it's not arriving the end of valid data
+      while(pos < Cur)
+      {
 
 				if(10 == pos)   //'A'
 				{
@@ -696,12 +700,14 @@ void WIFI_GetEvent(void)
 		//Continue to receive data
 		if(eStateMachine == EN_RECV_ST_GET_C_DATA)
 		{
+			TIM3_Int_Deinit();
 			pos = 0;
 			while(pos <= Cur)
       {
 				if((PackLen-3) == pos)   //接收数据长度结束
 				{
 					eStateMachine = EN_RECV_ST_GET_C_END;
+					TIM3_Int_Init(499,7199);
 					break;
 				}
 				pos++;
