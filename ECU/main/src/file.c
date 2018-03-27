45229aef5c4ae7b60f89cb641b472def57445a00
ShengfengDong <895961454@qq.com>
2018-03-27 13:29:20 +0800

    1.默认不存在地区
    Signed-off-by: ShengfengDong <895961454@qq.com>

--- ECU/main/src/file.c
+++ ECU/main/src/file.c
@@ -1430,7 +1430,7 @@ void initPath(void)
 	mkdir("/home/record/inversta",0x777);
 	mkdir("/home/record/power",0x777);
 	mkdir("/home/record/energy",0x777);
-	echo("/yuneng/area.con","NA");
+	//echo("/yuneng/area.con","NA");
 	echo("/yuneng/channel.con","0x10");
 	echo("/yuneng/limiteid.con","1");
 	echo("/yuneng/control.con","Timeout=10\nReport_Interval=15\nDomain=ecu.apsema.com\nIP=60.190.131.190\nPort1=8997\nPort2=8997\n");
